8778a6f2408f0a038678bb40541db6843c9495df
Rohit vishwakarma <rohitvvishwakarma400@email.com>
2025-07-03 12:42:52 -0400

    Update Swagger host for

--- docs/swagger.js
+++ docs/swagger.js
@@ -6,8 +6,8 @@ const doc = {
     title: 'E-commerce Product API',
     description: 'REST API with Node.js, Prisma, and NeonDB',
   },
-  host: 'https://ecommerce-1azq.onrender.com',   // ✅ Update this after deployment
-  schemes: ['https'],
+  host: 'localhost:3000', // ✅ Update this after deployment
+  schemes: ['http'],
 };
 
 const outputFile = './docs/swagger-output.json';
