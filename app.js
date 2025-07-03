808f8a4944cbcb2ac9b5eff8cf92702567c753f1
Rohit vishwakarma <rohitvvishwakarma400@email.com>
2025-07-03 10:05:28 -0400

    routes change

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const productRoutes = require('./routes/productRoutes');
 app.use(express.json());
 
 // Swagger UI route
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerFile));  //change ->  /api-docs
 
 // #swagger.tags = ['General']
 // #swagger.description = 'API Docs for Products and Categories'
